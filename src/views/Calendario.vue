0b3afc779eb13c8aea1feefc969ef1e943701b08
unknown <keat_9@hotmail.com>
2019-05-21 16:08:50 +0200

    calendario

--- src/views/Calendario.vue
+++ src/views/Calendario.vue
@@ -1,7 +1,83 @@
 <template>
   <v-container>
-    <v-layout justify-end>
-      <v-btn dark color="rgba(0, 0, 0, 0)" v-on="on"></v-btn>
+    <v-layout>
+      <div class="text-xs-center">
+        <v-chip color="primary" text-color="white">Urban Post</v-chip>
+
+        <v-chip color="orange" text-color="white">Articulos</v-chip>
+
+        <v-chip color="red" text-color="white">Eventos</v-chip>
+
+        <v-chip color="green" text-color="white">Videoclips</v-chip>
+
+        <v-chip color="purple" text-color="white">Entrevistas</v-chip>
+      </div>
+    </v-layout>
+
+    <v-layout wrap>
+      <v-flex xs12 class="mb-3">
+        <v-sheet height="500">
+          <v-calendar
+            show-month-on-first="false"
+            class="calendar"
+            :now="today"
+            :value="today"
+            v-model="today2"
+            end="2019-01-02"
+            color="primary"
+            ref="calendar"
+            :type="type"
+          >
+            <template v-slot:day="{ date }">
+              <template v-for="event in eventsMap[date]">
+                <v-menu :key="event.title" v-model="event.open" full-width offset-x>
+                  <template v-slot:activator="{ on }">
+                    <div
+                      v-if="!event.time"
+                      v-ripple
+                      :class="'ev ' + event.event"
+                      v-on="on"
+                      v-html="event.title"
+                    ></div>
+                  </template>
+                  <v-card color="white" min-width="350px" flat>
+                    <v-toolbar :class="event.event" dark>
+                      <v-btn icon>
+                        <v-icon>edit</v-icon>
+                      </v-btn>
+                      <v-toolbar-title v-html="event.title"></v-toolbar-title>
+                      <v-spacer></v-spacer>
+
+                      <v-btn icon>
+                        <v-icon>more_vert</v-icon>
+                      </v-btn>
+                    </v-toolbar>
+                    <v-card-title primary-title>
+                      <span v-html="event.details"></span>
+                    </v-card-title>
+                    <v-card-actions>
+                      <v-btn flat color="secondary">Cancel</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-menu>
+              </template>
+            </template>
+          </v-calendar>
+        </v-sheet>
+      </v-flex>
+
+      <v-flex sm4 xs12 class="text-sm-left text-xs-center">
+        <v-btn @click="$refs.calendar.prev()">
+          <v-icon dark left>keyboard_arrow_left</v-icon>Prev
+        </v-btn>
+      </v-flex>
+      <v-flex sm4 xs12 class="text-xs-center"></v-flex>
+      <v-flex sm4 xs12 class="text-sm-right text-xs-center">
+        <v-btn @click="$refs.calendar.next()">
+          Next
+          <v-icon right dark>keyboard_arrow_right</v-icon>
+        </v-btn>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -9,9 +85,93 @@
 <script>
 export default {
   data: () => ({
-    links: ["Contacto", "¿Quien Somos?"]
+    type: "month",
+    today2: "2019-05-17",
+    links: ["Contacto", "¿Quien Somos?"],
+    events: [
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2019-05-26",
+        open: false,
+        event: "evento"
+      },
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2018-12-31",
+        open: false,
+        event: "urbanpost"
+      },
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2019-01-01",
+        open: false,
+        event: "articulos"
+      },
+      {
+        title: "Meeting",
+        details: "Spending time on how we do not have enough time",
+        date: "2019-01-07",
+        open: false,
+        event: "entrevistas"
+      },
+      {
+        title: "30th Birthday",
+        details: "Celebrate responsibly",
+        date: "2019-01-03",
+        open: false,
+        event: "videoclips"
+      },
+      {
+        title: "New Year",
+        details: "Eat chocolate until you pass out",
+        date: "2019-01-01",
+        open: false,
+        event: "evento"
+      },
+      {
+        title: "Conference",
+        details: "Mute myself the whole time and wonder why I am on this call",
+        date: "2019-01-21",
+        open: false,
+        event: "evento"
+      },
+      {
+        title: "Hackathon",
+        details: "Code like there is no tommorrow",
+        date: "2019-02-01",
+        open: false,
+        event: "entrevistas"
+      }
+    ]
   }),
-  components: {}
+
+  computed: {
+    // convert the list of events into a map of lists keyed by date
+    eventsMap() {
+      const map = {};
+      this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e));
+      return map;
+    },
+    today() {
+      var d = new Date(),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      return [year, month, day].join("-");
+    }
+  },
+  methods: {
+    open(event) {
+      alert(event.title);
+    }
+  }
 };
 </script>
 <style>
@@ -24,5 +184,55 @@ export default {
   display: flex;
   justify-content: center;
   margin: 10px;
+}
+.ev {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  width: 100%;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+}
+.entrevistas {
+  background-color: purple !important;
+  color: white;
+  border: 1px solid purple;
+}
+
+.evento {
+  background-color: red !important;
+  color: white;
+  border: 1px solid red;
+}
+.urbanpost {
+  background-color: blue !important;
+  color: white;
+  border: 1px solid blue;
+}
+
+.articulos {
+  background-color: orange !important;
+  color: white;
+  border: 1px solid orange;
+}
+.videoclips {
+  background-color: green !important;
+  color: white;
+  border: 1px solid green;
+}
+
+.calendar {
+  width: 100%;
+}
+
+.theme--light.v-sheet {
+  color: black;
+}
+
+.v-sheet.theme--light {
+  margin: 0;
 }
 </style>
