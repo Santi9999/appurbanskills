4b26c804e0fe2792730cd4202b11a492ef4e6b88
unknown <keat_9@hotmail.com>
2019-05-30 13:36:38 +0200

    skills tv y iconos

--- src/App.vue
+++ src/App.vue
@@ -1,170 +1,150 @@
 <template>
   <v-app>
-    <div class="fondo">
-      <v-flex xs12>
-        <v-card-title class="px-0">
-          <v-toolbar fixed dark>
-            <v-menu max-height="50px" offset-y transition="slide-x-transition">
-              <template v-slot:activator="{ on }">
-                <v-btn :depressed="true" :fab="true" :small="true" @click="mostrar = !mostrar">
-                  <v-toolbar-side-icon></v-toolbar-side-icon>
-                </v-btn>
-              </template>
-            </v-menu>
-            <v-toolbar-title>
-              <img class="portada" :src="require('./assets/ImagenesHome/Portada.png')">
-            </v-toolbar-title>
-          </v-toolbar>
-          <v-navigation-drawer v-model="mostrar" dark temporary fixed>
-            <v-list class="pa-1">
-              <v-list-tile to="/">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/7.Home2.png"
-                      width="24"
-                      height="24"
-                    >Inicio
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/nacional">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/1.Nacional2.png"
-                      width="24"
-                      height="24"
-                    >Nacional
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/internacional">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/2.Internacional2.png"
-                      width="24"
-                      height="24"
-                    >Internacional
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/eventos">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/3.Eventos2.png"
-                      width="24"
-                      height="24"
-                    >Eventos
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/skillstv">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/4.SkillsTv2.png"
-                      width="24"
-                      height="24"
-                    >Skills TV
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/calendario">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/5.Calendario2.png"
-                      width="24"
-                      height="24"
-                    >Calendario
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-            <v-list class="pa-1">
-              <v-list-tile to="/urbanpost">
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    <img
-                      class="botones"
-                      src="./assets/ImagenesHome/6.UrbanPost2.png"
-                      width="24"
-                      height="24"
-                    >Urban Post
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <hr>
-            </v-list>
-          </v-navigation-drawer>
-          <!-- <router-link to="/" class="mt-5">
-            <img src="./assets/ImagenesHome/Logourban.png" width="310" height="210">
-          </router-link>-->
-        </v-card-title>
-      </v-flex>
-      <!-- <v-layout justify-center>
-        <v-flex xs4>
-          <a href="https://www.instagram.com/urbanskillsoficial/" target="_blank">
-            <img class="botoninsta" src="./assets/ImagenesHome/insta2.png" width="40" height="40">
-          </a>
+    <v-layout id="top">
+      <div class="fondo">
+        <v-flex xs12>
+          <v-card-title class="px-0">
+            <v-toolbar fixed dark>
+              <v-menu max-height="50px" offset-y transition="slide-x-transition">
+                <template v-slot:activator="{ on }">
+                  <v-btn :depressed="true" :fab="true" :small="true" @click="mostrar = !mostrar">
+                    <v-toolbar-side-icon></v-toolbar-side-icon>
+                  </v-btn>
+                </template>
+              </v-menu>
+              <v-toolbar-title>
+                <img class="portada" :src="require('./assets/ImagenesHome/Portada.png')">
+              </v-toolbar-title>
+            </v-toolbar>
+            <v-navigation-drawer v-model="mostrar" dark temporary fixed>
+              <v-list class="pa-1">
+                <v-list-tile to="/">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/7.Home2.png"
+                        width="24"
+                        height="24"
+                      >Inicio
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/nacional">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/1.Nacional2.png"
+                        width="24"
+                        height="24"
+                      >Nacional
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/internacional">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/2.Internacional2.png"
+                        width="24"
+                        height="24"
+                      >Internacional
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/eventos">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/3.Eventos2.png"
+                        width="24"
+                        height="24"
+                      >Eventos
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/skillstv">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/4.SkillsTv2.png"
+                        width="24"
+                        height="24"
+                      >Skills TV
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/calendario">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/5.Calendario2.png"
+                        width="24"
+                        height="24"
+                      >Calendario
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+              <v-list class="pa-1">
+                <v-list-tile to="/urbanpost">
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <img
+                        class="botones"
+                        src="./assets/ImagenesHome/6.UrbanPost2.png"
+                        width="24"
+                        height="24"
+                      >Urban Post
+                    </v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </v-list>
+            </v-navigation-drawer>
+          </v-card-title>
         </v-flex>
-        <v-flex xs4>
-          <a href="https://www.youtube.com/urbanskillsoficial" target="_blank">
-            <img
-              class="botonyoutube"
-              src="./assets/ImagenesHome/youtube2.png"
-              width="55"
-              height="40"
-            >
-          </a>
-        </v-flex>
-        <v-flex xs4>
-          <a href="https://www.facebook.com/urbanskillsoficial/" target="_blank">
-            <img class="botonface" src="./assets/ImagenesHome/face2.png" width="40" height="40">
-          </a>
-        </v-flex>
-      </v-layout>-->
 
-      <router-view></router-view>
+        <router-view></router-view>
+        <v-btn fab bottom right fixed small color="#ccb803" dark href="#top" class="toTop">
+          <v-icon dark>keyboard_arrow_up</v-icon>
+        </v-btn>
 
-      <v-footer class="mt-3" height="auto" color="yellow darken-1">
-        <v-layout justify-center row wrap>
-          <a href="mailto:urbanskillsoficial@gmail.com">
-            <v-btn color="black" flat round>Contacto</v-btn>
-          </a>
-          <v-btn to="/quiensomos" color="black" flat round>¿Quiénes Somos?</v-btn>
-          <v-flex yellow lighten-2 py-3 text-xs-center black--text xs12>
-            &copy;2019 —
-            <strong>S.Gamero&G.Alvarez</strong>
-          </v-flex>
-        </v-layout>
-      </v-footer>
-    </div>
+        <v-footer class="mt-3" height="auto" color="yellow darken-1">
+          <v-layout justify-center row wrap>
+            <a href="mailto:urbanskillsoficial@gmail.com">
+              <v-btn color="black" flat round>Contacto</v-btn>
+            </a>
+            <v-btn to="/quiensomos" color="black" flat round>¿Quiénes Somos?</v-btn>
+            <v-flex yellow lighten-2 py-3 text-xs-center black--text xs12>
+              &copy;2019 —
+              <strong>S.Gamero&G.Alvarez</strong>
+            </v-flex>
+          </v-layout>
+        </v-footer>
+      </div>
+    </v-layout>
   </v-app>
 </template>
 
@@ -229,5 +209,9 @@ hr {
 
 .v-navigation-drawer > .v-list .v-list__tile--active .v-list__tile__title {
   color: gold;
+}
+
+.toTop {
+  color: black;
 }
 </style>
--- src/views/SkillsTv.vue
+++ src/views/SkillsTv.vue
@@ -26,10 +26,41 @@
         </a>
       </v-flex>
     </v-layout>
+    <v-layout align-center column>
+      <iframe
+        name="play"
+        width="300"
+        height="300"
+        :src="`https://www.youtube.com/embed/${
+        videos[0].snippet.thumbnails.default.url.split('/')[4]
+      }`"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <h1 class="titulovideo">{{videos[0].snippet.title}}</h1>
+    </v-layout>
     <v-layout justify-center>
-      <v-flex xs12>
-        <v-card :key="video.title" v-for="video in videos">{{video.snippet.title}}</v-card>
-      </v-flex>
+      <v-carousel class="carouselvideos" height="150" :cycle="false">
+        <v-carousel-item v-for="(item,name) in carousel()" :key="name">
+          <v-layout justify-center row align-center>
+            {{item.length}}
+            <a
+              :href="getId(video)"
+              target="play"
+              :key="video.snippet.title"
+              v-for="video in item"
+            >
+              <img
+                width="100"
+                height="100"
+                :src="video.snippet.thumbnails.default.url"
+                frameborder="0"
+              >
+            </a>
+          </v-layout>
+        </v-carousel-item>
+      </v-carousel>
     </v-layout>
   </v-container>
 </template>
@@ -40,7 +71,8 @@ export default {
     links: ["Contacto", "¿Quien Somos?"],
     videos: [],
     url:
-      "https://www.googleapis.com/youtube/v3/activities?part=snippet&channelId=UCJ6f-Cjc8fWi-EKIZ1zHIWQ&maxResults=50&key=AIzaSyBz_jq_T4ahYmK0UlkpyfYkX4W-5m6PDl0"
+      "https://www.googleapis.com/youtube/v3/activities?part=snippet&channelId=UCJ6f-Cjc8fWi-EKIZ1zHIWQ&maxResults=12&key=AIzaSyBz_jq_T4ahYmK0UlkpyfYkX4W-5m6PDl0",
+    numeroVideos: 3
   }),
   methods: {
     getData() {
@@ -48,16 +80,38 @@ export default {
         .then(json => json.json())
         .then(data => {
           this.videos = data.items;
-          console.log(this.videos);
+          this.carousel();
         });
+    },
+    getId(video) {
+      return `https://www.youtube.com/embed/${
+        video.snippet.thumbnails.default.url.split("/")[4]
+      }`;
+    },
+
+    carousel() {
+      let carousel = {};
+      let paginas = Math.ceil(this.videos.length / this.numeroVideos);
+      let inicio = 0;
+      let final = this.numeroVideos;
+      for (let i = 0; i < paginas; i++) {
+        let corte = this.videos.slice(inicio, final);
+        carousel["slider" + i] = corte;
+
+        inicio = inicio + this.numeroVideos;
+        final = final + this.numeroVideos;
+      }
+      return carousel;
     }
   },
+  computed: {},
   created() {
     this.getData();
   }
 };
 </script>
 <style>
+@import url("https://fonts.googleapis.com/css?family=Lobster&display=swap");
 .px-0 {
   display: flex;
   justify-content: center;
@@ -71,5 +125,15 @@ export default {
 
 .v-card.v-sheet.theme--light {
   color: white;
+}
+
+.carouselvideos {
+  margin-top: 40px;
+}
+
+.titulovideo {
+  margin-top: 20px;
+  color: white;
+  font-family: "Lobster", cursive;
 }
 </style>
